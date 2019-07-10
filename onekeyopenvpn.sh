ee38ec43d9b03975dea9a05be472019f212d86db
mzh741 <21965911+mzh741@users.noreply.github.com>
2019-07-10 16:49:08 +0800

    Update onekeyopenvpn.sh

--- onekeyopenvpn.sh
+++ onekeyopenvpn.sh
@@ -108,7 +108,7 @@ cat > /etc/rc.d/init.d/openv<<-EOF
 #description:openv
 
 cd /usr/src/udp
-nohup ./speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:1194 -f2:4 --mode 0 --timeout 0 >speeder.log 2>&1 &
+nohup ./speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:1194 -f1:10 --mode 0 --timeout 0 >speeder.log 2>&1 &
 nohup ./udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &
 systemctl start openvpn@server
 EOF
